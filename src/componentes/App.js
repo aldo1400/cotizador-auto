a6f32daada012e4639c24019d4b3748d29f38e70
Aldo Choque <achoque1400@gmail.com>
2018-08-02 23:12:14 -0500

    helpers para cotizar el auto

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import Formulario from './Formulario';
+import {obtenerDiferenciaAnio, calcularMarca,obtenerPlan} from '../helper';
+
 
 class App extends Component {
 
     cotizarSeguro=(datos)=>{
-        console.log(datos);
+        const {marca,plan,year}=datos;
+
+        // Agregar una base de 2000
+
+        let resultado=2000;
+
+        // Obtener la diferencia de años y por cada año restar el 3%
+        
+        const diferencia=obtenerDiferenciaAnio(year);
+        console.log('La diferencia es '+ diferencia)
+
+        // por cada año restar el 3% al valor del seguro
+    
+        resultado-=((diferencia*3)*resultado)/100;
+        console.log(resultado);
+        // Americano 15% Asiatico 5% y europeo 30% de incremento al valor actual
+
+        resultado=calcularMarca(marca)*resultado;
+       
+        // el plan del auto, el basico incrementa el valor 20% y cobertura completa 50%
+
+        let incrementoPlan=obtenerPlan(plan);
+        console.log(incrementoPlan);
+        // dependiendo del plan incrementar
+
+        resultado=parseFloat(incrementoPlan*resultado).toFixed(2);
+        
+        console.log(resultado);
+
+        // ya tenemos el costo
     }
 
 
